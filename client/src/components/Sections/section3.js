f31cc09341c00cce9d5648e2b47a2466656a5bef
Michael Taylor <michaelntaylor@gmail.com>
2019-08-16 15:47:37 -0400

    add section3 and section4

--- client/src/components/Sections/section3.js
+++ client/src/components/Sections/section3.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './sections.scss';
+import sec3 from '../Assets/CloudComputing.png';
 
 class Section3 extends React.Component {
   render() {
     return (
       <>
         <div id='sectionThree' className='sectionThree'>
-          <p className='section'>this a section</p>
+          <div>
+            <img src={sec3} />
+          </div>
+          <div className='section'>
+            <h1>Cloud Computing</h1>
+            <p>
+              Cloud computing is the delivery of computing services over the
+              Internet to offer faster innovation and flexible resources. Some
+              of these services include servers, storage, databases, networking,
+              software, analytics and intelligence Cloud computing is a big
+              shift from the traditional way businesses think about IT
+              resources. Cloud services are popular because it helps lower your
+              operating costs, run infrastructure more efficiently and scale as
+              your business needs change. Students would be advised to look
+              deeper in cloud computing because of its popularity.
+            </p>
+          </div>
         </div>
       </>
     );
--- client/src/components/Sections/section4.js
+++ client/src/components/Sections/section4.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './sections.scss';
+import sec4 from '../Assets/DataAnalytics.png';
 
 class Section4 extends React.Component {
   render() {
     return (
       <>
         <div id='sectionFour' className='sectionFour'>
-          <p className='section'>this a section</p>
+          <div>
+            <div className='text-container'>
+              <h1>Data Analytics</h1>
+              <p className='section'>
+                Data analysis is a process of inspecting, cleansing,
+                transforming and modeling data with the goal of discovering
+                useful information, informing conclusions and supporting
+                decision-making. In today's business world, data analysis plays
+                a role in making decisions more scientific and helping
+                businesses operate more effectively.
+              </p>
+
+              <p>
+                Data analyis has been particularily adopted in the field of
+                medicine and contstruction. Students would be advised to look
+                deeper into these industries.
+              </p>
+            </div>
+          </div>
+          <div className='img4'>
+            <img src={sec4} />
+          </div>
         </div>
       </>
     );
